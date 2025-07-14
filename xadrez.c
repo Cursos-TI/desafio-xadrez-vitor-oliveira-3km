47076eb526d7c1539ea664be65032113a730c2d5
Vitor de Oliveira Araújo <109856517+vitor-oliveira-3km@users.noreply.github.com>
2025-07-14 10:24:46 -0300

    Update xadrez.c
    Adicionado o movimento do cavalo utilizando for e while

--- xadrez.c
+++ xadrez.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-
+// Jogo de xadrez
 int main(){
     
     int rainha = 0;
-
+    int cavalo, CavaloL;
+//Movimento da torre
     printf("Primeira peça a se movimentar é a torre! \n");
     for (int torre = 0; torre < 5; torre++)
     {
         printf("Direita \n");
     }
-    
+//Movimento do bispo
     printf("Proxima peça a se mover é o bispo! \n");
     for (int bispo = 0; bispo < 5; bispo++)
     {
         printf("Cima e direita \n");
     }
-
+//Movimento da rainha
     printf("Proxima peça a se mover é a rainha! \n");
 
 
@@ -30,7 +31,19 @@ int main(){
         
         rainha ++;
     }
-    
+//Movimento do cavalo
+    printf("Proxima peça a se mover é o cavalo! \n");
+    for (cavalo = 1; cavalo <= 1; cavalo++)
+    {
+        
+        while (CavaloL <= 1)
+        {
+            printf("Horizontal \n");
+            CavaloL++;
+        }
+        
+        printf("Esquerda\n");
+    }
 
     return 0;
 
